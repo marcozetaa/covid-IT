e93b26f60f088b19ec94dea1976538cfe829bc2b
Marco Zanghieri <marco.zanghieri19@gmail.com>
2020-05-18 19:02:49 +0200

    Upload regione

--- Regioni/regioniScriptQuery.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#component-regione').delay(2000).fadeOut();
-    $('#rightcolumn').delay(1000).fadeIn();
-    
-});
-    
--- Regioni/regioniScriptVue.js
+++ Regioni/regioniScriptVue.js
@@ -2,28 +2,32 @@ var regione = new Vue({
     el: '#component-regione',
     data: function() {
         return{
-        regioneSelezionata: 0,
-        regioni: [
-                {id:0, nomeRegione:'Abruzzo', info:"regione stocazzo", image:'regioniFoto/abruzzo.png'},
-                {id:1, nomeRegione:'Basilicata', info:"regione stocazzo", image:'regioniFoto/basilicata.png'},
-                {id:2, nomeRegione:'Calabria', info:"regione stocazzo", image:'regioniFoto/calabria.png'},
-                {id:3, nomeRegione:'Campania', info:"regione stocazzo", image:'regioniFoto/campania.png'},
-                {id:4, nomeRegione:'Emilia-Romagna', info:"regione stocazzo", image:'regioniFoto/emilia-romagna.png'},
-                {id:5, nomeRegione:'Friuli Venezia Giulia', info:"regione stocazzo", image:'regioniFoto/friuli-venezia-giulia.png'},
-                {id:6, nomeRegione:'Lazio', info:"regione stocazzo", image:'regioniFoto/lazio.png'},
-                {id:7, nomeRegione:'Liguria', info:"regione stocazzo", image:'regioniFoto/liguria.png'},
-                {id:8, nomeRegione:'Lombardia', info:"regione stocazzo", image:'regioniFoto/lombardia.png'},
-                {id:9, nomeRegione:'Marche', info:"regione stocazzo", image:'regioniFoto/marche.png'},
-                {id:10, nomeRegione:'Molise', info:"regione stocazzo", image:'regioniFoto/molise.png'},
-                {id:11, nomeRegione:'Piemonte', info:"regione stocazzo", image:'regioniFoto/piemonte.png'},
-                {id:12, nomeRegione:'Puglia', info:"regione stocazzo", image:'regioniFoto/puglia.png'},
-                {id:13, nomeRegione:'Sardegna', info:"regione stocazzo", image:'regioniFoto/sardegna.png'},
-                {id:14, nomeRegione:'Sicilia', info:"regione stocazzo", image:'regioniFoto/sicilia.png'},
-                {id:15, nomeRegione:'Toscana', info:"regione stocazzo", image:'regioniFoto/toscana.png'},
-                {id:16, nomeRegione:'Trentino Alto Adige', info:"regione stocazzo", image:'regioniFoto/trentino-alto-adige.png'},
-                {id:17, nomeRegione:'Umbria', info:"regione stocazzo", image:'regioniFoto/umbria.png'},
-                {id:18, nomeRegione:"Valle D'Aosta", info:"regione stocazzo", image:'regioniFoto/valle-daosta.png'},
-                {id:19, nomeRegione:'Veneto', info:"regione stocazzo", image:'regioniFoto/veneto.png'}
+            loading: true,
+            errored: false,
+            regioneSelezionata: 0,
+            regioni: [],
+            immagini: [
+                {id:0, image:'regioniFoto/abruzzo.png'},
+                {id:1, image:'regioniFoto/basilicata.png'},
+                {id:2, image:'regioniFoto/trentino-alto-adige.png'},
+                {id:3, image:'regioniFoto/calabria.png'},
+                {id:4, image:'regioniFoto/campania.png'},
+                {id:5, image:'regioniFoto/emilia-romagna.png'},
+                {id:6, image:'regioniFoto/friuli-venezia-giulia.png'},
+                {id:7, image:'regioniFoto/lazio.png'},
+                {id:8, image:'regioniFoto/liguria.png'},
+                {id:9, image:'regioniFoto/lombardia.png'},
+                {id:10, image:'regioniFoto/marche.png'},
+                {id:11, image:'regioniFoto/molise.png'},
+                {id:12, image:'regioniFoto/piemonte.png'},
+                {id:13, image:'regioniFoto/puglia.png'},
+                {id:14, image:'regioniFoto/sardegna.png'},
+                {id:15, image:'regioniFoto/sicilia.png'},
+                {id:16, image:'regioniFoto/toscana.png'},
+                {id:17, image:'regioniFoto/trentino-alto-adige.png'},
+                {id:18, image:'regioniFoto/umbria.png'},
+                {id:19, image:'regioniFoto/valle-daosta.png'},
+                {id:20, image:'regioniFoto/veneto.png'}
             ]
         };
     },
@@ -34,13 +38,72 @@ var regione = new Vue({
     },
     computed: {
         nomeRegione: function(){
-            return this.regioni[this.regioneSelezionata].nomeRegione;
+            return this.regioni[this.regioneSelezionata].denominazione_regione;
+        },
+        data: function(){
+            var data = this.regioni[this.regioneSelezionata].data;
+            var res = data.split("T"); 
+            return res[0] + ' - ' + res[1];
+        },
+        idRegione: function(){
+            return this.regioni[this.regioneSelezionata].codice_regione;
+        },
+        ricoveratiConSintomi: function(){
+            return this.regioni[this.regioneSelezionata].ricoverati_con_sintomi;
+        },
+        terapiaIntensiva: function(){
+            return this.regioni[this.regioneSelezionata].terapia_intensiva; 
+        },
+        totaleOspedalizzati: function(){
+            return this.regioni[this.regioneSelezionata].totale_ospedalizzati;
+        },
+        isolamentoDomiciliare: function(){
+            return this.regioni[this.regioneSelezionata].isolamento_domiciliare;
+        },
+        totalePositivi: function(){
+            return this.regioni[this.regioneSelezionata].totale_positivi;
+        },
+        variazioneTotalePositivi: function(){
+            var info = this.regioni[this.regioneSelezionata].variazione_totale_positivi;
+            if (info > 0) $('#variazione').css('color','red');
+            else $('#variazione').css('color','green');
+            return info;
+        },
+        nuoviPositivi: function(){
+            return this.regioni[this.regioneSelezionata].nuovi_positivi;
+        },
+        dimessiGuariti: function(){
+            return this.regioni[this.regioneSelezionata].dimessi_guariti;
+        },
+        totaleDeceduti: function(){
+            return this.regioni[this.regioneSelezionata].deceduti;
+        },
+        totaleCasi: function(){
+            return this.regioni[this.regioneSelezionata].totale_casi;
+        },
+        totaleTamponi: function(){
+            return this.regioni[this.regioneSelezionata].tamponi;
+        },
+        casiTestati: function(){
+            return this.regioni[this.regioneSelezionata].casi_testati;
+        },
+        noteIT: function(){
+            return this.regioni[this.regioneSelezionata].note_it;
         },
         info: function(){
-            return this.regioni[this.regioneSelezionata].info;
+            return this.immagini[this.regioneSelezionata].info;
         },
         image: function(){
-            return this.regioni[this.regioneSelezionata].image;
+            return this.immagini[this.regioneSelezionata].image;
         }
-    }
+    },
+    mounted () {
+            axios.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json')
+            .then(response => {this.regioni = response.data})
+            .catch(error => {
+                console.log(error)
+                this.errored=true;
+            })
+            .finally(() => this.loading=false);
+        }
 });
