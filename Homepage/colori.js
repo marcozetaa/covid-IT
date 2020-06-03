e92157587b709f4933d4c705523893ee6e9856eb
Benedetta31 <benedettaclaire.zelada@gmail.com>
2020-06-03 19:44:02 +0200

    glicolic

--- Homepage/colori.js
+++ Homepage/colori.js
@@ -3,15 +3,15 @@ function ripristina(e){
 }
 
 
-function coloreHeader(e){
+function coloreGiallo(e){
     e.target.style.color="rgb(235,184,29)";
 }
 
 function assegnaEvent(){
-    var span2=document.getElementsByClassName("navbar");
-    for(i=0;i<span2.length;i++){
-        span2[i].addEventListener("mouseenter", coloreHeader);
-        span2[i].addEventListener("mouseleave", ripristina);
+    var navbr=document.getElementsByClassName("navbar");
+    for(i=0;i<navbr.length;i++){
+        navbr[i].addEventListener("mouseenter", coloreGiallo);
+        navbr[i].addEventListener("mouseleave", ripristina);
     }
 
 }
