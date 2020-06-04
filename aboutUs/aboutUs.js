29c4b9c5440e58ca587fc0fe32b1a75221baa00b
Benedetta31 <benedettaclaire.zelada@gmail.com>
2020-06-04 14:55:56 +0200

    ok

--- aboutUs/aboutUs.js
+++ aboutUs/aboutUs.js
@@ -1,34 +1,8 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
 
 
 function valida(){
   if((document.mess.name.value=="") || (document.mess.email.value=="")){
-    swal("Devi inserire nome e email!","Non hai mandato il messaggio", "warning");
+    swal("Devi inserire nome e email!","Non hai mandato il messaggio", "error");
     return false;
   }
   swal("Dati inseriti correttamente!","Hai mandato il messaggio", "success");
